9a2cc3718f1e13310ace3f6da8722e87a2bb8f63
Bryan Croteau <bdcroteau@gmail.com>
2024-04-12 14:04:48 -0700

    Add more error messages

--- src/main/kotlin/moirai/service/Printing.kt
+++ src/main/kotlin/moirai/service/Printing.kt
@@ -58,37 +58,37 @@ fun localize(error: ErrorKind): String =
     when(error) {
         CalculateCostFailed -> "Cost calculation phase failed"
         is CannotExplicitlyInstantiate -> "Explicit type arguments are not supported for ${error.symbol.value}"
-        is CannotFindBestType -> "Cannot find best type"
+        is CannotFindBestType -> "Cannot find best type: ${error.types.map { it.value }.joinToString { ", " }}"
         CannotInstantiate -> "Cannot instantiate"
-        is CannotRefFunctionParam -> "Formal parameters with a function type cannot be referenced"
-        is CannotUsePlatformSumTypeMember -> "Usage of sum type member types is not allowed, use the sum type instead"
-        is CannotUseRawType -> "Cannot use raw type without type parameters"
+        is CannotRefFunctionParam -> "Formal parameter ${error.identifier.value} has a function type and cannot be referenced"
+        is CannotUsePlatformSumTypeMember -> "Usage of sum type member ${error.type.value} is not allowed, use the sum type instead"
+        is CannotUseRawType -> "Cannot use raw type without type ${error.type.value} parameters"
         CostOverLimit -> "The cost of executing the script exceeds the upper bound allowed by this architecture"
         DecimalInfiniteDivide -> "Divide by zero"
-        is DictionaryArgsMustBePairs -> "The arguments to the Dictionary constructor must have type Pair"
+        is DictionaryArgsMustBePairs -> "The arguments to the Dictionary constructor must have type Pair, actual: ${error.actual.value}"
         is DuplicateCaseDetected -> "Duplicate case ${error.name}"
         is DuplicateImport -> "Duplicate import ${error.import.joinToString { "." }}"
         is DuplicateTypeParameter -> "Duplicate type parameter ${error.identifier.value}"
         ExpectOtherError -> "Invalid state, expect other error"
         ExpectedNamedScript -> "A named script was expected, but a transient script was observed"
         is ExpectedTransientScript -> "A transient script was expected, but the named script ${error.name} was observed"
-        is FinMismatch -> "Pessimistic upper bound mismatch, expected ${error.expected}, actual ${error.actual}"
+        is FinMismatch -> "Pessimistic upper bound mismatch, expected: ${error.expected}, actual: ${error.actual}"
         is ForEachFeatureBan -> "Source type ${error.type.value} is not supported when using the for loop"
-        is FormalParamFeatureBan -> "This type cannot appear as a formal parameter to a function"
-        is FunctionAssign -> "Functions cannot appear in the assign statement"
+        is FormalParamFeatureBan -> "This type ${error.type.value} cannot appear as a formal parameter to a function"
+        is FunctionAssign -> "Functions cannot appear in the assign statement, local ${error.identifier.value}"
         is FunctionReturnType -> "Function types cannot be used as the return type of a function"
         is IdentifierAlreadyExists -> "Identifier ${error.identifier.value} already exists"
-        is IdentifierCouldNotBeDefined -> TODO()
-        is IdentifierNotFound -> TODO()
-        is ImmutableAssign -> TODO()
-        is ImpossibleState -> TODO()
-        is IncompatibleString -> TODO()
-        is IncorrectNumberOfArgs -> TODO()
-        is IncorrectNumberOfTypeArgs -> TODO()
-        is IndexOutOfBounds -> TODO()
-        InvalidAssign -> TODO()
-        is InvalidCostExpressionFunctionName -> TODO()
-        InvalidCostUpperLimit -> TODO()
+        is IdentifierCouldNotBeDefined -> "Identifier ${error.identifier.value} could not be defined"
+        is IdentifierNotFound -> "Identifier ${error.signifier.value} not be found"
+        is ImmutableAssign -> "Attempt to assign to immutable symbol ${error.symbol.value}"
+        is ImpossibleState -> "Impossible state: ${error.msg}"
+        is IncompatibleString -> "Type ${error.type.value} is not compatible with String"
+        is IncorrectNumberOfArgs -> "Incorrect number of arguments, expected: ${error.expected}, actual: ${error.actual}"
+        is IncorrectNumberOfTypeArgs -> "Incorrect number of type arguments, expected: ${error.expected}, actual: ${error.actual}"
+        is IndexOutOfBounds -> "Index out of bounds, index: ${error.index}, size: ${error.size}"
+        InvalidAssign -> "Invalid assign"
+        is InvalidCostExpressionFunctionName -> "Only Sum, Min, and Max functions are supported in cost expressions, actual: ${error.name}"
+        InvalidCostUpperLimit -> "Invalid cost upper limit"
         is InvalidDefinitionLocation -> TODO()
         is InvalidFinLiteral -> TODO()
         is InvalidFinTypeSub -> TODO()
