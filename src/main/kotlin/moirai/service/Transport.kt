001d67d77b642d6a0f43f163c8c86ea2b98358bb
Bryan Croteau <bdcroteau@gmail.com>
2024-04-17 16:13:51 -0700

    Add collection support

--- src/main/kotlin/moirai/service/Transport.kt
+++ src/main/kotlin/moirai/service/Transport.kt
@@ -33,13 +33,88 @@ fun jsonToMoirai(node: JsonNode, type: TransportType): String {
             jsonFail()
         }
 
-        is TransportObjectType -> TODO()
-        is TransportParameterizedBasicType -> TODO()
-        is TransportParameterizedRecordType -> TODO()
-        is TransportPlatformObjectType -> TODO()
-        is TransportPlatformSumObjectType -> TODO()
-        is TransportPlatformSumRecordType -> TODO()
-        is TransportPlatformSumType -> TODO()
+        is TransportObjectType -> {
+            if (node.isTextual && node.asText() == type.name) {
+                return type.name
+            }
+
+            jsonFail()
+        }
+
+        is TransportParameterizedBasicType -> {
+            if (type.name == "String" && node.isValueNode) {
+                val t = node.toString().trimStart('"').trimEnd('"')
+                return "\"${t}\""
+            }
+
+            if (type.name == "Decimal" && node.isValueNode) {
+                return node.asDouble().toString()
+            }
+
+            if (type.name == "List" && node.isArray) {
+                val elements = node.elements().asSequence().toList().map { jsonToMoirai(it, type.typeArgs.first()) }
+                return "List(${elements.joinToString { ", " }})"
+            }
+
+            if (type.name == "Set" && node.isArray) {
+                val elements = node.elements().asSequence().toList().map { jsonToMoirai(it, type.typeArgs.first()) }
+                return "Set(${elements.joinToString { ", " }})"
+            }
+
+            if (type.name == "Dictionary" && node.isArray) {
+                val elements = node.elements().asSequence().toList().map {
+                    if (it.isPojo || it.isObject) {
+                        val fn = it.fieldNames().asSequence().toList()
+                        if (fn.size != 2) {
+                            jsonFail()
+                        }
+
+                        // TODO: Define your semantics for dictionaries
+                        // Sorted field names alphabetically
+                        val s = fn.sorted()
+                        // First is key
+                        val k = it[s[0]]
+                        // Second is value
+                        val v = it[s[1]]
+
+                        "${jsonToMoirai(k, type.typeArgs[0])} to ${jsonToMoirai(v, type.typeArgs[1])}"
+                    }
+
+                    jsonFail()
+                }
+            }
+
+            jsonFail()
+        }
+
+        is TransportParameterizedRecordType -> {
+            if (node.isObject || node.isPojo) {
+                val fields = type.fields.map { jsonToMoirai(node[it.name], it.type) }.joinToString { ", " }
+                return "${type.name}($fields)"
+            }
+
+            jsonFail()
+        }
+
+        is TransportPlatformObjectType -> {
+            if (node.isTextual && node.asText() == type.name) {
+                return type.name
+            }
+
+            jsonFail()
+        }
+
+        is TransportPlatformSumType -> {
+            if (type.name == "Option") {
+                if (node.isNull) {
+                    return "None"
+                } else {
+                    return "Some(${jsonToMoirai(node, type.typeArgs.first())})"
+                }
+            }
+
+            jsonFail()
+        }
 
         NonPublicTransportType,
         TransportConstantFin,
@@ -49,6 +124,8 @@ fun jsonToMoirai(node: JsonNode, type: TransportType): String {
         is TransportProductCostExpression,
         is TransportSumCostExpression,
         is TransportFunctionType,
+        is TransportPlatformSumObjectType,
+        is TransportPlatformSumRecordType,
         is TransportStandardTypeParameter -> jsonFail()
     }
 }
